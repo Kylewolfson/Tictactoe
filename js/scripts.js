d90dbeafac3a67c9b5a695659e78577eb524862e
Ian Lesikar and Kyle Wolfson <ian@>
2016-04-07 14:15:24 -0700

    created undefeatable, hopefully, AI opponent

--- js/scripts.js
+++ js/scripts.js
@@ -47,6 +47,20 @@ newBoard.reset();
 //   }
 // }
 
+Board.prototype.bestAvailable = function() {
+  if (newBoard.spaces[4].playerMark === false) {return 4}
+  if (newBoard.spaces[0].playerMark === false) {return 0}
+  if (newBoard.spaces[2].playerMark === false) {return 2}
+  if (newBoard.spaces[6].playerMark === false) {return 6}
+  if (newBoard.spaces[8].playerMark === false) {return 8}
+
+}
+Board.prototype.preventFork = function() {
+  if (playerCounter === 3) {
+    if (newBoard.spaces[1].playerMark === false) {return 1}
+    if (newBoard.spaces[3].playerMark === false) {return 3}
+  }
+}
 Board.prototype.canIWin = function() {
   var winningArrays =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
   for(var i = 0; i < winningArrays.length; i++) {
@@ -59,6 +73,25 @@ Board.prototype.canIWin = function() {
     if (newBoard.spaces[space1].playerMark === "Player O") {winningSpaces++;}
     if (newBoard.spaces[space2].playerMark === "Player O") {winningSpaces++;}
     if (newBoard.spaces[space3].playerMark === "Player O") {winningSpaces++;}
+    if (newBoard.spaces[space1].playerMark === false) {emptySpaces++; emptySpace = space1;}
+    if (newBoard.spaces[space2].playerMark === false) {emptySpaces++; emptySpace = space2;}
+    if (newBoard.spaces[space3].playerMark === false) {emptySpaces++; emptySpace = space3;}
+    if ((winningSpaces === 2) && (emptySpaces === 1)) {return emptySpace}
+  }
+}
+
+Board.prototype.canIBlock = function() {
+  var winningArrays =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+  for(var i = 0; i < winningArrays.length; i++) {
+    var winningSpaces = 0;
+    var emptySpaces = 0;
+    space1 = winningArrays[i][0];
+    space2 = winningArrays[i][1];
+    space3 = winningArrays[i][2];
+    emptySpace = false;
+    if (newBoard.spaces[space1].playerMark === "Player X") {winningSpaces++;}
+    if (newBoard.spaces[space2].playerMark === "Player X") {winningSpaces++;}
+    if (newBoard.spaces[space3].playerMark === "Player X") {winningSpaces++;}
     if (newBoard.spaces[space1].playerMark === false) {emptySpaces++; emptySpace = space1;}
     if (newBoard.spaces[space2].playerMark === false) {emptySpaces++; emptySpace = space2;}
     if (newBoard.spaces[space3].playerMark === false) {emptySpaces++; emptySpace = space3;}
@@ -126,6 +159,15 @@ $(function() {
           randomPick = Math.floor((Math.random() * 9));
         }
         if ((aiLevel === "hard") && (playerCounter % 2 === 1) && (newBoard.gameOver() == false)) {
+          debugger;
+            randomPick = newBoard.bestAvailable();
+          if (newBoard.preventFork()) {
+            randomPick = newBoard.preventFork();
+          }
+          if (newBoard.canIBlock()) {
+            randomPick = newBoard.canIBlock();
+          }
+
             if (newBoard.canIWin()) {
               randomPick = newBoard.canIWin();
             }
